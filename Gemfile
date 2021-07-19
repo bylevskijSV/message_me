011e7d0b49a6948c834765f4a686e83909579bb9
BylevskijSV <sergeibylewskii@gmail.com>
2021-07-19 22:23:40 +0300

    add semantic ui, jquery

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'webpacker', '~> 5.0'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
+# Sass-powered version of Semantic UI and ready to drop into Rails, Compass, or Sprockets. https://github.com/doabit/semantic-ui-sass
+gem 'semantic-ui-sass', '~> 2.4', '>= 2.4.4.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+require("jquery")
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass'
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require("packs/custom")
+
